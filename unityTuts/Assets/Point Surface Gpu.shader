4ac4baedf79eacfcb31df71f7d6f9eb6b035601d
mikser <miika.puonti@gmail.com>
2025-08-18 20:43:54 +0300

    tutorial_commentPass

--- unityTuts/Assets/Point Surface Gpu.shader
+++ unityTuts/Assets/Point Surface Gpu.shader
@@ -6,35 +6,58 @@ Shader "Graph/Point surface GPU"
         LOD 200
 
         CGPROGRAM
-        // Physically based Standard lighting model, and enable shadows on all light types
+        // configureSurface means having a surface kernel, Fullforwarshadows are shadow things and addshadow means we add a custom shadow pass
         #pragma surface ConfigureSurface Standard FullForwardShadows addshadow
-        #pragma editor_sync_compilation
-        // // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma editor_sync_compilation 
+        //make sure this specific shader is always compiled ahead of time, as the dummy shader could cause issues due to procedural drawing
         #pragma instancing_options procedural:ConfigureProcedural assumeuniformscaling 
 
-        #pragma target 4.5
+        #pragma target 4.5 //for compute shader & buffer support
+
+        float _Step;
 
         struct Input{
             float3 worldPos;
          };
-         float _Step;
+
+         
          #if defined (UNITY_PROCEDURAL_INSTANCING_ENABLED)
          StructuredBuffer<float3> _Positions;
          #endif
+
+
          void ConfigureProcedural(){
             #if defined (UNITY_PROCEDURAL_INSTANCING_ENABLED)
             float3 position = _Positions[unity_InstanceID];
-            unity_ObjectToWorld = 0.0;
+
+            unity_ObjectToWorld = 0.0; //initialize object to world conversion matrix
+            //this transformation will then be applied for each vertex i believe
+            //im notating down what the matrix should look like at each step just so i understand it
+            //for object position offset = p and step = s 
+            // 0   0   0   0
+            // 0   0   0   0
+            // 0   0   0   0
+            // 0   0   0   0
+
             unity_ObjectToWorld._m03_m13_m23_m33 = float4(position, 1.0);
+            //0   0   0   p   
+            //0   0   0   p   
+            //0   0   0   p   
+            //0   0   0   p   
+
             unity_ObjectToWorld._m00_m11_m22 = _Step;
-
+            //s   0   0   p   
+            //0   s   0   p   
+            //0   0   s   p   
+            //0   0   0   p
             #endif
-
 
          }
          void ConfigureSurface(Input input, inout SurfaceOutputStandard surface){
-			// surface.Albedo.rb = saturate(input.worldPos.y * (0.51) - 0.49);
-            surface.Albedo.rgb = saturate(input.worldPos.xyz * (0.5) + 0.5);
+            //same surface things as before 
+
+			surface.Albedo.rb = saturate(input.worldPos.y * (0.51)-.2);
+            //surface.Albedo.rgb = saturate(input.worldPos.xyz * (0.5) + 0.5);
 
             // surface.Smoothness = saturate(input.worldPos.y);
          }
--- unityTuts/Assets/Script/GPUgraph.cs
+++ unityTuts/Assets/Script/GPUgraph.cs
@@ -4,38 +4,41 @@ using UnityEngine.UIElements;
 
 public class GPUgraph : MonoBehaviour
 {
+    //for the gpu to have as references to cube object
     [SerializeField]
     Material material;
     [SerializeField]
     Mesh mesh;
 
-    ComputeBuffer positionBuffer;
+
+    ComputeBuffer positionBuffer; // reference to a cpu data buffer, not yet initialized
     [SerializeField]
     ComputeShader cShader;
-    static readonly int positionsId = Shader.PropertyToID("_Positions"),
-    resolutionId = Shader.PropertyToID("_Resolution"),
-		stepId = Shader.PropertyToID("_Step"),
-		timeId = Shader.PropertyToID("_Time");
 
+    //used to set properties for compute shader
+    static readonly int positionsId = Shader.PropertyToID("_Positions"),    //starting cube positions id, for the buffer
+                        resolutionId = Shader.PropertyToID("_Resolution"),
+		                stepId = Shader.PropertyToID("_Step"), // step between cubes
+		                timeId = Shader.PropertyToID("_Time");
+        
 
     //points per side
     [SerializeField, Range (5, 1000)]
     int resolution;
 
-    //select which function to visualize in the inspector
+    //NOT CURRENTLY USED, from Graph.cs, eventually these will also be replaced to work on gpu
     [SerializeField]
     FunctionLibrary.FunctionName function;
-
     [SerializeField, Min(0f)]
     float functionDuration = 1f;
     bool transitioning;
     FunctionLibrary.FunctionName transFunction;
-
-
     float currDuration;
+    //END OF UNUSED
 
      void OnEnable()
     {
+        //count is the total amount of cubes, "stride" or the size of each element is 3 3D positions, each taking 4 bytes, so 3*4
         positionBuffer = new ComputeBuffer(resolution * resolution, 3*4);
     }
     private void Update()
@@ -44,24 +47,34 @@ public class GPUgraph : MonoBehaviour
     }
     private void OnDisable()
     {
+        //free gpu memory
         positionBuffer.Release();
-        positionBuffer = null;
+        positionBuffer = null; // enables the garbage collector to free this even quicker as the reference is null
 
     }
     void UpdateFuncGPU()
     {
+        //distance between cubes
         float step = 2f / resolution;
+
+        //give the shader new property values
         cShader.SetInt(resolutionId, resolution);
         cShader.SetFloat(stepId, step);
         cShader.SetFloat (timeId, Time.time);
+        cShader.SetBuffer(0,positionsId,positionBuffer); //buffer also takes a kernel as argument
 
-        cShader.SetBuffer(0,positionsId,positionBuffer);
+        //function kernel takes fixed groups of 8 * 8, so we need to dispatch x/8 * y/8 times
         int groups = Mathf.CeilToInt(resolution / 8f);
-        cShader.Dispatch(0, groups, groups, 1);
+        cShader.Dispatch(0, groups, groups, 1); // dispatch to calcucate positions
 
-        material.SetBuffer(positionsId, positionBuffer);
+        //give material access to the now compute shadered values to draw from
+        material.SetBuffer(positionsId, positionBuffer); 
         material.SetFloat(stepId, step);
-        var bounds = new Bounds(Vector3.zero, Vector3.one* (2.0f + 2.0f / resolution));
+
+        //boundingBox is needed for culling
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2.0f + 2.0f / resolution)); //based on points remaining within a cube of size 2, with some potentially reaching outside it by half a sizes 
+
+        //draw call
         Graphics.DrawMeshInstancedProcedural(mesh, 0, material,bounds, positionBuffer.count);
     }
 
