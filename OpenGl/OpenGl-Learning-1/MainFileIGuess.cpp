d3f396c89e26551b70a5ef8f7d3ce760cae45265
mikser <miika.puonti@gmail.com>
2026-01-01 18:37:36 +0200

    OPENGL: refactored things slightly

--- OpenGl/OpenGl-Learning-1/MainFileIGuess.cpp
+++ OpenGl/OpenGl-Learning-1/MainFileIGuess.cpp
@@ -30,6 +30,59 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
+void CompileShader(unsigned int shader, const char* source) {
+	glShaderSource(shader, 1, &source, NULL);
+	glCompileShader(shader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" <<
+			infoLog << std::endl;
+	}
+}
+
+void CreateShaders(
+	unsigned int & VBO, unsigned int & VAO, unsigned int& shaderProgram) {
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//copy vertex data to buffer
+	//THIS VERSION STILL HARDCODES USING THE VERTECIES OBJECT!!s
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader	= glCreateShader(GL_VERTEX_SHADER);
+	unsigned int fragmentShader	= glCreateShader(GL_FRAGMENT_SHADER);
+
+	CompileShader(vertexShader,		vertexShaderSource);
+	CompileShader(fragmentShader,	fragmentShaderSource);
+
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader); glDeleteShader(vertexShader);
+
+
+
+}
+void  setupVertexArrays(unsigned int & VAO, unsigned int &VBO) {
+	glGenVertexArrays(1, &VAO);
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+		(void*)0);
+	glEnableVertexAttribArray(0);
+}
 
 
 int main() {
@@ -62,68 +115,14 @@ int main() {
 	//called every time user resizes window, so that things function still yay
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//get a vertex buffer object
+
+	//create shader program::
 	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//copyy vertex data to buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-	//create vertex shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	 
-	//compile
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-
-
-	//verify compiles successfull
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
-			infoLog << std::endl;
-	}
-	int success2;
-	char infoLog2[512];
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success2);
-	if (!success2) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog2);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
-			infoLog2 << std::endl;
-	}
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	
-	glDeleteShader(vertexShader); glDeleteShader(vertexShader);
-
+	unsigned int shaderProgram;
 	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	// 1. bind Vertex Array Object
-	glBindVertexArray(VAO);
-	// 2. copy our vertices array in a buffer for OpenGL to use
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 3. then set our vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-		(void*)0);
-	glEnableVertexAttribArray(0);
 
-
-
-
+	CreateShaders(VBO, VAO, shaderProgram);
+	setupVertexArrays(VAO, VBO);
 
 	//render loop
 	while (!glfwWindowShouldClose(window)) {
